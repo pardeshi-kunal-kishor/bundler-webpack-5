91bd04c98823bef8aa72c5a69ae89fae38bf96c6
kunal pardeshi <pardeshi.kunal.kishor@gmail.com>
2023-11-17 00:15:03 +0530

    handling scss

--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,10 @@ module.exports = {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
     ],
   },
 };
