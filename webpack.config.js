b60de5e37e371a32248b9bfb6618df1647055b1e
kunal pardeshi <pardeshi.kunal.kishor@gmail.com>
2023-11-17 01:37:43 +0530

    html-webpack-plugin plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,14 @@ const path = require("path");
 const TerserPlugin = require("terser-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "bundle.[contenthash].js",
     path: path.resolve(__dirname, "./dist"),
-    publicPath: "dist/",
+    publicPath: "",
   },
   mode: "none",
   module: {
@@ -66,5 +67,6 @@ module.exports = {
         path.join(process.cwd(), "build/**/*"), // to clean additional folders apart from dist.
       ],
     }),
+    new HtmlWebpackPlugin(),
   ],
 };
